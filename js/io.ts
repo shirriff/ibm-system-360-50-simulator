0cc90aea776f7e129aad375c184d30d0036b71fd
Ken Shirriff <ken.shirriff@gmail.com>
2019-04-08 17:58:59 -0700

    Switch to TypeScript. Compile with 'tsc'.

---
+++ js/io.ts
@@ -0,0 +1,142 @@
+// i/o simulation code
+
+var ioactions = {}; // Dictionary of pending I/O actions
+
+var count; // Instruction count from simulator
+
+function doio(state, entry) {
+  if (count in ioactions) {
+    var status = ioactions[count](state);
+    delete ioactions[count];
+    return status;
+  }
+}
+
+var cards = [];
+var cardIdx = 0;
+var cardOffset = 0; // In 4-byte units for now
+var cards1 = [
+          [0x00000000, 0x00000400, 0x02000400, 0x00000050, 0x00020000, 0x00000000],
+          [0x05340000, 0x00000000, 0x00000000] // branch and link
+          ];
+
+function loadCards(cardfile) {
+  cards = [];
+  for (var i = 0; i < cardfile.length; i++) {
+    var card = cardfile[i];
+    for (var j = card.length; j < 80; j++) {
+      card[j] = 0;
+    }
+    cards.push(card);
+  }
+  cardIdx = 0;
+  cardOffset = 0;
+}
+
+loadCards(cards1);
+
+// Read cards from CCW(s)
+// Returns status message
+function doReadFromCCW(state, ccwAddr, chainDataCommand?) {
+  if (ccwAddr & 3) {
+    alert('doReadFromCCW alignment problem: ' + ccwAddr);
+  }
+  var ccw = state['MS'][ccwAddr];
+  var ccwB= state['MS'][ccwAddr + 4];
+  var command = ccw >>> 24;
+  if (chainDataCommand != undefined) {
+    command = chainDataCommand;
+  }
+  var dataAddr = ccw & 0xffffff;
+  var flags = (ccwB >>> 27) & 0xff;
+  if (flags & 1) {
+    alert('Unexpected PCI flag');
+  }
+  var count = ccwB & 0xffff;
+  if (dataAddr & 3) {
+    alert('doReadFromCCW address alignment problem: ' + dataAddr);
+  }
+  if (ccwAddr & 3) {
+    alert('doReadFromCCW count alignment problem: ' + count);
+  }
+  for (var i = 0; i < count; i += 4) {
+    if (cardOffset >= cards[cardIdx].length) {
+      if (flags & 32) { // SILI flag
+        break;
+      } else {
+        alert('Length error: ' + i + ' vs ' + count);
+        break;
+      }
+    }
+    if (!(flags & 2)) { // if no skip flag
+      state['MS'][dataAddr + i] = cards[cardIdx][cardOffset];
+    }
+    cardOffset += 1;
+  }
+  var msg = 'I/O: Read ' + i + ' to ' + dataAddr;
+  if (flags & 2) {
+    msg += ' (skip)';
+  }
+  if (flags & 16) { // Chain data
+    return msg + '\n' + doReadFromCCW(state, ccwAddr + 8, command) + ' (chain data)';
+  } else if (flags & 8) { // Chain command
+    cardIdx += 1; // Move to next card
+    cardOffset = 0;
+    return msg + '\n' + doReadFromCCW(state, ccwAddr + 8) + ' (chain command)';
+  } else {
+    cardIdx += 1; // Move to next card
+    cardOffset = 0;
+    return msg;
+  }
+}
+
+function chctl(state, entry) {
+  switch (state['W']) {
+    case 0:
+      // 0000 -> CHCTL: proc with IRPT QK700
+      break;
+    case 1:
+      // 0001 -> CHCTL: start IO QK800:09b4 QK700:9be
+      ioactions[count+2] = function(state) {
+        state['S'][3] = 1; // Channel response QK800:0988
+        state['CR'] = 0; // for CRMD. 0 = CC for start IO accepted? QK800
+        return "IO accepted";
+      };
+      break;
+    case 2:
+      // 0010 -> CHCTL: halt IO QK700 (time out? QK701:0942)
+      alert('io.js: chctl ' + state['W'] + ' not implemented');
+      break;
+    case 4:
+      // 0100 -> CHCTL: test IO QK800:09b4 (emit time out QK702:090d)
+      ioactions[count+2] = function(state) {
+        state['S'][3] = 1; // Channel response QK800:0988
+        state['S'][2] = 1; // Channel end status received QK800:09b5
+        // 50Maint page 156: CC=1 for channel end, CC=2 for channel busy
+        var us = 0x6c; // Unit status must be 0xx01x00 for success
+        var cs = 0x80; // Channel status must be xx000000 for success
+        state['M'] = (us << 24) | (cs << 16);
+        // CCW2 stored in word 1, so set up word 0
+        state['MS'][0] = 0x02000000; // Read, addr = 0
+        return doReadFromCCW(state, 0);
+      };
+      break;
+    case 8:
+      // 1000 -> CHCTL: test chan QK700
+      alert('io.js: chctl ' + state['W'] + ' not implemented');
+      break;
+    case 16:
+      // 00010000 -> CHCTL: issue foul on start QK700:09bc
+      alert('io.js: chctl ' + state['W'] + ' not implemented');
+      break;
+    default:
+      alert('io.js: chctl ' + state['W'] + ' not implemented');
+      break;
+  }
+}
+
+function ech(state, entry) {
+  alert('io.js: e->ch not implemented');
+}
+
+
---
+++ js/utils.ts
@@ -0,0 +1,53 @@
+// Share utility functions
+
+function createState() {
+  // Initialize arbitrarily
+  var state = {'FN': 3, 'J': 3, 'LSAR': 3,
+  'SYSMASK': 0, 'KEY': 0, 'AMWP': 0, 'IRUPT': 0, 'ILC': 0, 'CC': 0, 'PROGMASK': 0, 'IAR': 0, // PSW
+  'L': 0xffffffff, 'R': 0xffffffff, 'MD': 3, 'F': 3, 'Q': 1,
+  'M': 0xffffffff, 'H': 0xffffffff, 'T': 3,
+  'A': 3, 'D': 3, 'XG': 3, 'Y': 3, 'U': 3, 'V': 3, 'W': 3,
+  'G1': 3, 'G2': 3, 'LB': 3, 'MB': 3, 'SP': 5,
+  'WFN': 2, // Set up at QK801:0988 during IPL
+  'SAR': 0xffffff, 'SDR': 0xffffffff,
+  'ROAR': 0xffff, 'BS': [0, 0, 0, 0],
+  };
+  state['LS'] = new Array(64);
+  for (var i = 0; i < 64; i++) {
+    state['LS'][i] = 0x01010101 * i;
+  }
+  state['MS'] = new Array(8192).fill(0); // Words
+  state['S'] = new Array(8).fill(0); // Words
+  return state;
+}
+
+// Convert string to hex roar address string
+function fmtAddress(d) {
+  return d.toString(16).padStart(4, '0').toLowerCase();
+}
+
+// Format d as a bit
+function fmtB(d) {
+  return d.toString(2);
+}
+
+// Format d as a hex nybble
+function fmtN(d) {
+  return d.toString(16);
+}
+
+// Format d as 1 hex byte
+function fmt1(d : number) {
+  return d.toString(16).padStart(2, '0');
+}
+
+// Format d as 2 hex bytes
+function fmt2(d) {
+  return d.toString(16).padStart(4, '0');
+}
+
+// Format d as 4 hex bytes
+function fmt4(d) {
+  return d.toString(16).padStart(8, '0');
+}
+
